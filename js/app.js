12d9dde19ff1e72c0e43bfbbb15cd6f415ba788d
Llewellyn Dawson <llewellyn@myitmanager.co.za>
2016-06-27 16:48:27 +0200

    New SS test

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 (function(){
     'use strict';
 
-    var module = angular.module('app', ['onsen', 'ngMap', 'ngSanitize', 'ngCordova']);
+    var module = angular.module('app', ['onsen', 'ngMap', 'ngSanitize']);
 
     // form file directive (Assist with file uploads)
     module.directive('fileModel', ['$parse', function ($parse) {
@@ -20,6 +20,145 @@
             }
         };
     }]);
+
+    module.factory('$cordovaSocialSharing', ['$q', '$window', function ($q, $window) {
+
+        return {
+          share: function (message, subject, file, link) {
+            var q = $q.defer();
+            subject = subject || null;
+            file = file || null;
+            link = link || null;
+            $window.plugins.socialsharing.share(message, subject, file, link, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareViaTwitter: function (message, file, link) {
+            var q = $q.defer();
+            file = file || null;
+            link = link || null;
+            $window.plugins.socialsharing.shareViaTwitter(message, file, link, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareViaWhatsApp: function (message, file, link) {
+            var q = $q.defer();
+            file = file || null;
+            link = link || null;
+            $window.plugins.socialsharing.shareViaWhatsApp(message, file, link, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareViaFacebook: function (message, file, link) {
+            var q = $q.defer();
+            message = message || null;
+            file = file || null;
+            link = link || null;
+            $window.plugins.socialsharing.shareViaFacebook(message, file, link, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareViaFacebookWithPasteMessageHint: function (message, file, link, pasteMessageHint) {
+            var q = $q.defer();
+            file = file || null;
+            link = link || null;
+            $window.plugins.socialsharing.shareViaFacebookWithPasteMessageHint(message, file, link, pasteMessageHint, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareViaSMS: function (message, commaSeparatedPhoneNumbers) {
+            var q = $q.defer();
+            $window.plugins.socialsharing.shareViaSMS(message, commaSeparatedPhoneNumbers, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareViaEmail: function (message, subject, toArr, ccArr, bccArr, fileArr) {
+            var q = $q.defer();
+            toArr = toArr || null;
+            ccArr = ccArr || null;
+            bccArr = bccArr || null;
+            fileArr = fileArr || null;
+            $window.plugins.socialsharing.shareViaEmail(message, subject, toArr, ccArr, bccArr, fileArr, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          shareVia: function (via, message, subject, file, link) {
+            var q = $q.defer();
+            message = message || null;
+            subject = subject || null;
+            file = file || null;
+            link = link || null;
+            $window.plugins.socialsharing.shareVia(via, message, subject, file, link, function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          canShareViaEmail: function () {
+            var q = $q.defer();
+            $window.plugins.socialsharing.canShareViaEmail(function () {
+              q.resolve(true);
+            }, function () {
+              q.reject(false);
+            });
+            return q.promise;
+          },
+
+          canShareVia: function (via, message, subject, file, link) {
+            var q = $q.defer();
+            $window.plugins.socialsharing.canShareVia(via, message, subject, file, link, function (success) {
+              q.resolve(success);
+            }, function (error) {
+              q.reject(error);
+            });
+            return q.promise;
+          },
+
+          available: function () {
+            var q = $q.defer();
+            window.plugins.socialsharing.available(function (isAvailable) {
+              if (isAvailable) {
+                q.resolve();
+              }
+              else {
+                q.reject();
+              }
+            });
+
+            return q.promise;
+          }
+        };
+     }]);
 
     module.filter('externalLinks', function() {
         return function(text) {
@@ -1361,38 +1500,45 @@
         };
         
         //Social Sharing
-        $cordovaSocialSharing
-            .shareViaTwitter(message, image, link)
-            .then(function(result) {
-              // Success!
-            }, function(err) {
-              // An error occurred. Show a message to the user
-        });
+        $scope.shareViaTwitter = function(message, image, link) {
+            $cordovaSocialSharing
+                .shareViaTwitter(message, image, link)
+                .then(function(result) {
+                  // Success!
+                }, function(err) {
+                  // An error occurred. Show a message to the user
+            });
+        }
+        
+        $scope.shareViaTwitter = function(message, image, link) {
+            $cordovaSocialSharing
+                .shareViaWhatsApp(message, image, link)
+                .then(function(result) {
+                  // Success!
+                }, function(err) {
+                  // An error occurred. Show a message to the user
+            });
+        }
+        
+        $scope.shareViaFacebook = function(message, image, link) {
+            $cordovaSocialSharing
+                .shareViaFacebook(message, image, link)
+                .then(function(result) {
+                  // Success!
+                }, function(err) {
+                  // An error occurred. Show a message to the user
+            });
+        }
 
-        $cordovaSocialSharing
-            .shareViaWhatsApp(message, image, link)
-            .then(function(result) {
-              // Success!
-            }, function(err) {
-              // An error occurred. Show a message to the user
-        });
-
-        $cordovaSocialSharing
-            .shareViaFacebook(message, image, link)
-            .then(function(result) {
-              // Success!
-            }, function(err) {
-              // An error occurred. Show a message to the user
-        });
-
-        // toArr, ccArr and bccArr must be an array, file can be either null, string or array
-        $cordovaSocialSharing
-            .shareViaEmail(message, subject, toArr, ccArr, bccArr, file)
-            .then(function(result) {
-              // Success!
-            }, function(err) {
-              // An error occurred. Show a message to the user
-        });
+        $scope.shareViaFacebook = function(message, subject, toArr, ccArr, bccArr, file) {
+            $cordovaSocialSharing
+                .shareViaEmail(message, subject, toArr, ccArr, bccArr, file)
+                .then(function(result) {
+                  // Success!
+                }, function(err) {
+                  // An error occurred. Show a message to the user
+            });
+        }
     });
     
     module.controller('mapController', function($scope, NgMap, $timeout) {
